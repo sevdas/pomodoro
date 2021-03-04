d534bda9812d8b111b81fc3894021ed54dc10137
sevdas <sshukrieva@gmail.com>
2021-03-04 15:35:01 +0000

    set target goal counter

--- timer.js
+++ timer.js
@@ -1,6 +1,7 @@
 const focusButton = document.querySelector('.focus')
 
 let sound = new Audio("birds-voice.mp3")
+let roundCounter = 0
 
 function startFocus(){
     minutes = 24
@@ -10,8 +11,8 @@ function startFocus(){
     document.querySelector('.seconds').textContent = seconds
 
 
-    let minutesInterval = setInterval(minutesTimer,6000) /* 1minute is equal to 60000 miliseconds */
-    let secondsInterval = setInterval(secondsTimer, 100)
+    let minutesInterval = setInterval(minutesTimer,600) /* 1minute is equal to 60000 miliseconds */
+    let secondsInterval = setInterval(secondsTimer, 10)
     
 
     function minutesTimer(){
@@ -32,15 +33,33 @@ function startFocus(){
 
                   document.querySelector('.message').textContent = 'Take a short break'
                   document.querySelector('.message').classList.add('show-message') /*to make an element visible*/
-                  document.querySelector('.counter')
+            
                 
-
                   sound.play()
+                  roundCounter++
+                  updateCount()
                }
                seconds = 60
+
             }   
         }
     }
 
 focusButton.addEventListener('click', startFocus)
 
+const targetData = document.querySelector('.target')
+console.log(targetData)
+
+const updateCount = () => {
+    const target = +targetData.getAttribute('data-target') //convert data type from string to number by adding + sign to the front of the variable targetData
+    const count = roundCounter
+
+    if(count < target){
+      targetData.textContent = roundCounter
+      setTimeout(updateCount, 1) // update the count after 1 milisecond.
+    } else {
+        targetData.textContent = target
+        document.querySelector('.message').textContent = 'Welldone! You hit the max level of productivity!'
+        document.querySelector('.message').style.backgroundColor = ' #20bf6b'
+    }
+}
