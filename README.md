e5f3d53d3390f4011654a416cf98e1fb5f45b993
sevdas <sshukrieva@gmail.com>
2021-08-22 14:53:26 +0100

    add fades out animation to the circle

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
-# sevdas-timer
+# Pre-course 4 Mini-project - POMODORO TIMER
 
-## Pre-course 4 Mini-project
-
-This week we are building pomodoro which is a time management technique made out of spaced repetition system. Do the work, take a break.
+This week we are building Pomodoro which is a time management technique made out of spaced repetition system. Do the work, take a break.
 
 Every 3 repetitions will create a set. After each set we will take a long break.
-We will also set a daily goal of 12 repetiitons to hit the maximum productivity level!
+We will also set a daily goal of 12 repetitions to hit the maximum productivity level!
 The timer will count down to zero and once it hits zero the bell feature will ring to notify the user either to take a break or to get focused.
 
 ---
 
+### User Story:
+
+- A “work” timer that counts down from 25 to zero minutes.
+- A second “break” timer that counts down from 5 to zero minutes.
+- Buttons to start a session, pause the timer, or cancel the session and restart.
+- Play an alarm sound to make it obvious the time is up.
+
+### How I approached it;
+
 - I've start styling my timer with universal selector (\*) to reset default browser settings.
 - I used viewport units to set the color to the full browser height and width so the size is based on the users entire viewport.
 - To display / manipulate inner texts I used `.textContent` property after watched [this video](https://www.youtube.com/watch?v=1UsllDMhvN4) explaining how does it allows user input on the webpage.
-- This challenge helped me to visualise the async funtions in real life scenario.
+- This challenge helped me to visualize the async functions in real life scenario.
 - Aside from manipulating data of hours, minutes and seconds, I also had a chance of making use of clearInterval and setInterval.
 
       <blockquote>
--- timer.js
+++ timer.js
@@ -1,225 +1,229 @@
 //Return button elements from the document
-const focusButton = document.querySelector('.focus')
-const shortBreakButton = document.querySelector('.break-short')
-const longBreakButton = document.querySelector('.break-long')
-const resetButton = document.querySelector('.reset')
+const focusButton = document.querySelector(".focus");
+const shortBreakButton = document.querySelector(".break-short");
+const longBreakButton = document.querySelector(".break-long");
+const resetButton = document.querySelector(".reset");
 
-//Global Vartiables
-let sound = new Audio("birds-voice.mp3")
-let roundCounter = 0 //Focus counter
-let countShortbreaks = 0 //Short break counter
+//Global Variables
+let sound = new Audio("birds-voice.mp3");
+let roundCounter = 0; //Focus counter
+let countShortbreaks = 0; //Short break counter
 let minutesInterval;
-let secondsInterval
-let minutes
-let seconds
+let secondsInterval;
+let minutes;
+let seconds;
 
-
-'use strict'
+("use strict");
 /* FOCUS EVENT ---------------------------------------------------------------------------------------------------------- */
-function startFocus(){
+function startFocus() {
+  //Reset
+  clearIntervals();
 
-    //Reset
-    clearIntervals()
+  minutes = "24";
+  seconds = "59";
+  document.querySelector(".minutes").textContent = minutes;
+  document.querySelector(".seconds").textContent = seconds;
 
-    minutes = '24'
-    seconds = '59'
-    document.querySelector('.minutes').textContent = minutes
-    document.querySelector('.seconds').textContent = seconds
-    
-    minutesInterval = setInterval(minutesTimer,60000) /* 1minute is equal to 60000 miliseconds */
-    secondsInterval = setInterval(secondsTimer, 1000)
+  minutesInterval = setInterval(
+    minutesTimer,
+    60000
+  ); /* 1minute is equal to 60000 milliseconds */
+  secondsInterval = setInterval(secondsTimer, 1000);
 
-    //Reset the display message
-    document.querySelector('.message').textContent = ''
-    document.querySelector('.message').classList.remove('show-message') /*to make an element visible*/
-    
-   //Minutes timer when user clicks on FOCUS button
-    function minutesTimer(){
-        minutes = ('0' + (minutes - 1)).slice(-2)
-        document.querySelector('.minutes').textContent = minutes
-       
+  //Reset the display message
+  document.querySelector(".message").textContent = "";
+  document
+    .querySelector(".message")
+    .classList.remove("show-message"); /*to make an element visible*/
+
+  //Minutes timer when user clicks on FOCUS button
+  function minutesTimer() {
+    minutes = ("0" + (minutes - 1)).slice(-2);
+    document.querySelector(".minutes").textContent = minutes;
+  }
+  function secondsTimer() {
+    seconds = ("0" + (seconds - 1)).slice(-2);
+    document.querySelector(".seconds").textContent = seconds;
+
+    /*to prevent infinity*/
+    if (seconds <= 0) {
+      if (minutes <= 0) {
+        /* stop interval */
+        clearInterval(minutesInterval);
+        clearInterval(secondsInterval);
+
+        document.querySelector(".message").textContent = "Take a short break";
+        document
+          .querySelector(".message")
+          .classList.add("show-message"); /* to make an element visible */
+
+        sound.play();
+        roundCounter++;
+        updateCount();
+      }
+      seconds = 60;
     }
-        function secondsTimer(){
-            seconds = ('0' + (seconds - 1)).slice(-2)
-            document.querySelector('.seconds').textContent = seconds
-
-            /*to prevent infinity*/
-            if(seconds <= 0){
-               if(minutes <= 0){
-                  /* stop interval */
-                  clearInterval(minutesInterval)
-                  clearInterval(secondsInterval)
-
-                  document.querySelector('.message').textContent = 'Take a short break'
-                  document.querySelector('.message').classList.add('show-message') /*to make an element visible*/
-
-                
-                  sound.play()
-                  roundCounter++
-                  updateCount()
-    
-               }
-               seconds = 60
-
-            }   
-        }
-    }
-    focusButton.addEventListener('click', startFocus)
-
-
-//Set daily target of 12 sets for every 25 min of interval. 
-const targetData = document.querySelector('.target')
-const updateCount = () => {
-    const target = +targetData.getAttribute('data-target') //convert data type from string to number by adding + sign to the front of the variable targetData
-    const count = roundCounter
-
-    if(count < target){
-      targetData.textContent = roundCounter
-      setTimeout(updateCount, 1) // update the count after 1 milisecond.
-    } else {
-        targetData.textContent = target
-        document.querySelector('.message').textContent = 'Welldone! You Hit the Max Level of Productivity!'
-        document.querySelector('.message').style.backgroundColor = '#20bf6b'
-
-    }
+  }
 }
+focusButton.addEventListener("click", startFocus);
 
+//Set daily target of 12 sets for every 25 min of interval.
+const targetData = document.querySelector(".target");
+const updateCount = () => {
+  const target = +targetData.getAttribute("data-target"); //convert data type from string to number by adding + sign to the front of the variable targetData
+  const count = roundCounter;
 
+  if (count < target) {
+    targetData.textContent = roundCounter;
+    setTimeout(updateCount, 1); // update the count after 1 milisecond.
+  } else {
+    targetData.textContent = target;
+    document.querySelector(".message").textContent =
+      "Welldone! You Hit the Max Level of Productivity!";
+    document.querySelector(".message").style.backgroundColor = "#20bf6b";
+  }
+};
 
 /* SHORT BREAK EVENT -------------------------------------------------------------------------------------------------------------- */
-function shortBreak(){
-    //Reset
-    clearIntervals()
+function shortBreak() {
+  //Reset
+  clearIntervals();
 
-   minutes = '05'
-   seconds = '59'
-   document.querySelector('.minutes').textContent = minutes
-   document.querySelector('.seconds').textContent = seconds
+  minutes = "05";
+  seconds = "59";
+  document.querySelector(".minutes").textContent = minutes;
+  document.querySelector(".seconds").textContent = seconds;
 
-   minutes = ('0' + (minutes - 1)).slice(-2)
-   document.querySelector('.minutes').textContent = minutes
-    
-    minutesInterval = setInterval(minutesTimer,60000) /* 1minute is equal to 60000 miliseconds */
-    secondsInterval = setInterval(secondsTimer, 1000)
+  minutes = ("0" + (minutes - 1)).slice(-2);
+  document.querySelector(".minutes").textContent = minutes;
 
-    //Reset the display message
-    document.querySelector('.message').textContent = ''
-    document.querySelector('.message').classList.remove('show-message') /*to make an element visible*/
-    
-//    Minutes timer when user clicks on 'SHORT BREAK' button
-    function minutesTimer(){
-        minutes = ('0' + (minutes - 1)).slice(-2)
-        document.querySelector('.minutes').textContent = minutes
-       
-    }
-        function secondsTimer(){
-            seconds = ('0' + (seconds - 1)).slice(-2)
-            document.querySelector('.seconds').textContent = seconds
+  minutesInterval = setInterval(
+    minutesTimer,
+    60000
+  ); /* 1minute is equal to 60000 milliseconds */
+  secondsInterval = setInterval(secondsTimer, 1000);
 
-            /*to prevent infinity*/
-            if(seconds <= 0){
-               if(minutes <= 0){
-                  /* stop interval */
-                  clearInterval(minutesInterval)
-                  clearInterval(secondsInterval)
+  //Reset the display message
+  document.querySelector(".message").textContent = "";
+  document
+    .querySelector(".message")
+    .classList.remove("show-message"); /*to make an element visible*/
 
-                  //After the third round, display message directing to LONG BREAK.
-                  countShortbreaks++
+  //    Minutes timer when user clicks on 'SHORT BREAK' button
+  function minutesTimer() {
+    minutes = ("0" + (minutes - 1)).slice(-2);
+    document.querySelector(".minutes").textContent = minutes;
+  }
+  function secondsTimer() {
+    seconds = ("0" + (seconds - 1)).slice(-2);
+    document.querySelector(".seconds").textContent = seconds;
 
-                  if(countShortbreaks === 3){
-                    document.querySelector('.message').textContent = 'Time for Long Break!'
-                    document.querySelector('.message').classList.add('show-message') /*to make an element visible*/
-                  } else {
-                    document.querySelector('.message').textContent = 'Time to Focus!'
-                    document.querySelector('.message').classList.add('show-message') /*to make an element visible*/
-                  }
+    /*to prevent infinity*/
+    if (seconds <= 0) {
+      if (minutes <= 0) {
+        /* stop interval */
+        clearInterval(minutesInterval);
+        clearInterval(secondsInterval);
 
-                  sound.play()
-               }
-               seconds = 60
-            }
+        //After the third round, display message directing to LONG BREAK.
+        countShortbreaks++;
+
+        if (countShortbreaks === 3) {
+          document.querySelector(".message").textContent =
+            "Time for Long Break!";
+          document
+            .querySelector(".message")
+            .classList.add("show-message"); /*to make an element visible*/
+        } else {
+          document.querySelector(".message").textContent = "Time to Focus!";
+          document
+            .querySelector(".message")
+            .classList.add("show-message"); /*to make an element visible*/
         }
+
+        sound.play();
+      }
+      seconds = 60;
     }
-               shortBreakButton.addEventListener('click', shortBreak)
-
-
-
+  }
+}
+shortBreakButton.addEventListener("click", shortBreak);
 
 /* LONG BREAK EVENT -------------------------------------------------------------------------------------------------------------- */
-function longBreak(){
+function longBreak() {
+  //Reset
+  clearIntervals();
 
-   //Reset
-   clearIntervals()
+  minutes = "15";
+  seconds = "59";
+  document.querySelector(".minutes").textContent = minutes;
+  document.querySelector(".seconds").textContent = seconds;
 
-   minutes = '15'
-   seconds = '59'
-   document.querySelector('.minutes').textContent = minutes
-   document.querySelector('.seconds').textContent = seconds
+  minutes = ("0" + (minutes - 1)).slice(-2);
+  document.querySelector(".minutes").textContent = minutes;
 
-   minutes = ('0' + (minutes - 1)).slice(-2)
-   document.querySelector('.minutes').textContent = minutes
-    
-    minutesInterval = setInterval(minutesTimer,60000) /* 1minute is equal to 60000 miliseconds */
-    secondsInterval = setInterval(secondsTimer, 1000)
+  minutesInterval = setInterval(
+    minutesTimer,
+    60000
+  ); /* 1minute is equal to 60000 milliseconds */
+  secondsInterval = setInterval(secondsTimer, 1000);
 
-    //Reset the display message
-    document.querySelector('.message').textContent = ''
-    document.querySelector('.message').classList.remove('show-message') /*to make an element visible*/
-    
-//    Minutes timer when user clicks on 'SHORT BREAK' button
-    function minutesTimer(){
-        minutes = ('0' + (minutes - 1)).slice(-2)
-        document.querySelector('.minutes').textContent = minutes
-       
-    }
-        function secondsTimer(){
-            seconds = ('0' + (seconds - 1)).slice(-2)
-            document.querySelector('.seconds').textContent = seconds
+  //Reset the display message
+  document.querySelector(".message").textContent = "";
+  document
+    .querySelector(".message")
+    .classList.remove("show-message"); /* to make an element visible */
 
-            /*to prevent infinity*/
-            if(seconds <= 0){
-               if(minutes <= 0){
-                  /* stop interval */
-                  clearInterval(minutesInterval)
-                  clearInterval(secondsInterval)
+  //    Minutes timer when user clicks on 'SHORT BREAK' button
+  function minutesTimer() {
+    minutes = ("0" + (minutes - 1)).slice(-2);
+    document.querySelector(".minutes").textContent = minutes;
+  }
+  function secondsTimer() {
+    seconds = ("0" + (seconds - 1)).slice(-2);
+    document.querySelector(".seconds").textContent = seconds;
 
-                    document.querySelector('.message').textContent = 'Time to Focus!'
-                    document.querySelector('.message').classList.add('show-message') /*to make an element visible*/
-  
+    /*to prevent infinity*/
+    if (seconds <= 0) {
+      if (minutes <= 0) {
+        /* stop interval */
+        clearInterval(minutesInterval);
+        clearInterval(secondsInterval);
 
-                  sound.play()
-               }
-               seconds = 60
-            }
-        }
-    }
-    longBreakButton.addEventListener('click', longBreak)
+        document.querySelector(".message").textContent = "Time to Focus!";
+        document
+          .querySelector(".message")
+          .classList.add("show-message"); /* to make an element visible */
 
-
-    /* RESET EVENT -------------------------------------------------------------------------------------------------------------- */
-    function resetTimer(){
-        minutes = '00'
-        seconds = '00'
-        clearIntervals()  
-        document.querySelector('.minutes').textContent = minutes
-        document.querySelector('.seconds').textContent = seconds  
-    }
-
-    function clearIntervals(){
-        clearInterval(minutesInterval)
-        clearInterval(secondsInterval)    
-    }
-    resetButton.addEventListener('click', resetTimer)
-
-    function clickPrimaryButton(e){
-        let target = e.target
-        console.log(target)
-        console.log('you clicked the secondary button');
+        sound.play();
       }
+      seconds = 60;
+    }
+  }
+}
+longBreakButton.addEventListener("click", longBreak);
 
-  
+/* RESET EVENT -------------------------------------------------------------------------------------------------------------- */
+function resetTimer() {
+  minutes = "00";
+  seconds = "00";
+  clearIntervals();
+  document.querySelector(".minutes").textContent = minutes;
+  document.querySelector(".seconds").textContent = seconds;
+}
+
+function clearIntervals() {
+  clearInterval(minutesInterval);
+  clearInterval(secondsInterval);
+}
+resetButton.addEventListener("click", resetTimer);
+
+function clickPrimaryButton(e) {
+  let target = e.target;
+  console.log(target);
+  console.log("you clicked the secondary button");
+}
+
 //to do:
 //add fade out feature
-//display - You better not play with buttons! -  if user clicks to another button duting the session!
-
-      
+//display - You better not play with buttons! -  if user clicks to another button during the session!
